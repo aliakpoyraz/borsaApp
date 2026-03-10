9938470cb609c6bbca55c815ce7e240ba8e84190
Ali Akpoyraz <128495051+aliakpoyraz@users.noreply.github.com>
2026-03-11 00:55:39 +0300

    Borsa App Şablon

---
+++ Test/borsaAppTests/borsaAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  borsaAppTests.swift
+//  borsaAppTests
+//
+//  Created by Ali Akpoyraz on 11.03.2026.
+//
+
+import Testing
+@testable import borsaApp
+
+struct borsaAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
